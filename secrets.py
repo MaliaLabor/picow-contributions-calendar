003527021561ed125f08dcd728360e089df2256e
MaliaLabor <malia.labor@hotmail.com>
2023-02-09 11:08:21 -0700

    Add main.py and secrets.py
    Initial commit of python files

---
+++ secrets.py
@@ -0,0 +1,11 @@
+secrets = {
+    'ssid': 'WIFI SSID',
+    'pw': 'WIFI PASSWORD',
+    'username': 'GITHUB USERNAME',
+    'color_rgb': (0, 255, 0), # desired color in RGB values
+    'max_contributions_number': 35, # what number shows max brightness
+    'pixels_width': 32,
+    'pixels_height': 8,
+    'refresh_time_hours': 6, # how often the data will refresh
+    'brightness': 0.65 # brightness limiter to keep current draw low on led matrix
+    }
